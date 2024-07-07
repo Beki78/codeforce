8b0631b1ba5b8fcd6d50d11fb2382703796cbe3e
Baki78 <bakiget78@gmail.com>
2024-07-07 10:42:45 +0300

    done

---
+++ Medium_Number.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/contest/1760/problem/A
+
+
+for i in range(int(input())):
+    lst = list(map(int,input().split()))
+
+    lst.sort()
+
+    print(lst[1])
+    
+               
